1c1c9d483371e0addd5b33121a8a6b54884c8bce
Aditya Gupta <adityagupta107107107@gmail.com>
2025-02-11 17:07:03 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453 ;
+let accountEmail = "hello@gmail.com" ;
+var accountPassword = "12345" ;
+accountCity = "Mumbai" ;
+let accountState ;
+
+// accountId = 2 ;
+
+accountEmail = "hc@hc.com" ;
+accountPassword = "21212121" ;
+accountCity = "Delhi" ;
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]) ;
