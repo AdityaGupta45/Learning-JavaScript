ef5f5d7ffc6e4171f79cac76925244c8ca50c6c4
Aditya Gupta <adityagupta107107107@gmail.com>
2025-02-13 16:39:01 +0000

    containe config done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -12,5 +12,23 @@
 
 let someNumber = 99 ;
 let stringNumber = String(someNumber) ;
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+// **************Operations!!!!!!!!!!!!!!!
+
+let value = 5 ;
+let negValue = -value ;
+
+// console.log(negValue)
+
+let str1 = "hello" ; 
+let str2 = " Aditya" ;
+
+let str3 = str1 + str2 ;
+console.log(str3) ;
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
