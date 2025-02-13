ef882e93eb5dae17853b3857a8c82b070bbb6d6a
Aditya Gupta <adityagupta107107107@gmail.com>
2025-02-13 16:24:27 +0000

    Conversion of Data Types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+// let score = "33abc" ;
+
+// console.log(typeof score) ;
+// console.log(typeof(score)) ;
+
+// let valueInNumber = Number(score) ;
+// console.log(valueInNumber)
+
+// let isLoggedIn = "aditya" ;
+// let bolleanIsLoggedIn = Boolean (isLoggedIn);
+// console.log(bolleanIsLoggedIn) ;
+
+let someNumber = 99 ;
+let stringNumber = String(someNumber) ;
+console.log(stringNumber);
+console.log(typeof stringNumber)
