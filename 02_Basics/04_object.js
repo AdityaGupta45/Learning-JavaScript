deaa219626b27a7faf21c0356cbc3fba627c42c1
Aditya Gupta <adityagupta107107107@gmail.com>
2025-04-08 17:24:37 +0000

    container config done

---
+++ 02_Basics/04_object.js
@@ -0,0 +1,48 @@
+// const tinderUser = new object()
+const tinderUser = {};
+tinderUser.id = "123abc";
+tinderUser.name = "sammy";
+tinderUser.isLoggedIn = false;
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "aditya@google.com",
+  fullName: {
+    userFullName: {
+      firstName: "Aditya",
+      lastName: "Gupta",
+    },
+  },
+};
+console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+const obj4 = { 5: "a", 6: "b" };
+
+// const obj3 = {obj1, obj2};
+// const obj3 = Object.assign({}, obj1, obj2, obj4);
+const obj3 = { ...obj1, ...obj2 };
+
+// console.log(obj3);
+const user = [
+  {
+    id: 1,
+    email: "a@gmail.com",
+  },
+  {
+    id: 1,
+    email: "a@gmail.com",
+  },
+  {
+    id: 1,
+    email: "a@gmail.com",
+  },
+];
+user[1].email;
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
