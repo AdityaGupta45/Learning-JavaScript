798ab97c7ca3693b1579e3c5c148e547c364ab50
Aditya Gupta <adityagupta107107107@gmail.com>
2025-04-15 17:52:45 +0000

    container config done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,34 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "february";
+
+switch (month) {
+    case "january":
+        console.log("January");
+        
+        break;
+
+    case "february":
+        console.log("Feb");
+        
+        break;
+    case "march":
+        console.log("Mar");
+        
+        break;
+    case "april":
+        console.log("Apr");
+        
+        break;
+
+    default:
+        console.log("Default case Match")
+        break;
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,23 @@
+// const userEmail = "a@aditya.ai"
+
+// if(userEmail){
+//     console.log("User email found");
+    
+// }
+// else{
+// console.log("User Email Did not found")
+// }
+
+
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 1
+
+
+// console.log(val1)
+
+
+const iceTeaPrice = 100
+
+iceTeaPrice >= 80 ? console.log("less than 80 ") : console.log("more than 80");
